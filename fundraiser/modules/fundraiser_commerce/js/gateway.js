c54b7560b1568314f8cc8e0d5d3ae5c6cd99bfbb
klapalme <kl.jr@aol.com>
2017-03-21 14:28:40 -0400

    update rules config groups field location

--- fundraiser/modules/fundraiser_commerce/js/gateway.js
+++ fundraiser/modules/fundraiser_commerce/js/gateway.js
@@ -1,9 +1,13 @@
 Drupal.behaviors.fundraiserGateway = {
-  attach: function(context) { (function($) {
-    $(document).ready(function() {
-      $("#edit-submit").click(function(){
-        return confirm("Please review your changes carefully. A misconfiguration can cause transactions to fail.\n\nAre you sure you want to proceed?");
+  attach: function (context) {
+    (function ($) {
+      $(document).ready(function () {
+        $("#edit-submit").once('processed', function () {
+          $("#edit-submit").click(function () {
+            return confirm("Please review your changes carefully. A misconfiguration can cause transactions to fail.\n\nAre you sure you want to proceed?");
+          });
+        });
       });
-    });
-  })(jQuery);}
+    })(jQuery);
+  }
 };
